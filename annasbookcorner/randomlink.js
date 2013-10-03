bacd680185440cb263ab12d29642134ef23bf99f
Anna Shipman <anna@annashipman.co.uk>
2013-10-03 18:46:08 +0100

    Add old site - Anna's Book Corner :o)

---
+++ annasbookcorner/randomlink.js
@@ -0,0 +1,47 @@
+<!-- Begin
+function go_to(url) {
+window.location=url;
+}
+function rand_link() {
+var a;
+a = 1+Math.round(Math.random()*38);   // a = random number between 1-32
+if (a==1) go_to("/annasbookcorner/thriller.htm#Bunker13");
+if (a==2) go_to("/annasbookcorner/historical.htm#1933bad");
+if (a==3) go_to("/annasbookcorner/contemporary.htm#AllSummer");
+if (a==4) go_to("/annasbookcorner/nonfiction.htm#AmericanExorcism");
+if (a==5) go_to("/annasbookcorner/relationship.htm#BlissStreet");
+if (a==6) go_to("/annasbookcorner/thriller.htm#Bunker13");
+if (a==7) go_to("/annasbookcorner/satire.htm#DeadFamous");
+if (a==8) go_to("/annasbookcorner/relationship.htm#DensitySouls");
+if (a==9) go_to("/annasbookcorner/shorts.htm#FictionalScotland");
+if (a==10) go_to("/annasbookcorner/relationship.htm#FoxyT");
+if (a==11) go_to("/annasbookcorner/historical.htm#FrankieStankie");
+if (a==12) go_to("/annasbookcorner/contemporary.htm#JumpingGreen");
+if (a==13) go_to("/annasbookcorner/contemporary.htm#LifePi");
+if (a==14) go_to("/annasbookcorner/relationship.htm#Loverslover");
+if (a==15) go_to("/annasbookcorner/relationship.htm#Madame");
+if (a==16) go_to("/annasbookcorner/thriller.htm#MakingLove");
+if (a==17) go_to("/annasbookcorner/satire.htm#MeMe");
+if (a==18) go_to("/annasbookcorner/thriller.htm#Millionaires");
+if (a==19) go_to("/annasbookcorner/shorts.htm#MurielSpark");
+if (a==20) go_to("/annasbookcorner/nonfiction.htm#OtherWords");
+if (a==21) go_to("/annasbookcorner/contemporary.htm#Oxygen");
+if (a==22) go_to("/annasbookcorner/shorts.htm#PersonalVelocity");
+if (a==23) go_to("/annasbookcorner/historical.htm#PianoTuner");
+if (a==24) go_to("/annasbookcorner/nonfiction.htm#PublicProperty");
+if (a==25) go_to("/annasbookcorner/shorts.htm#RealTime");
+if (a==26) go_to("/annasbookcorner/relationship.htm#RightThing");
+if (a==27) go_to("/annasbookcorner/contemporary.htm#Self");
+if (a==28) go_to("/annasbookcorner/contemporary.htm#SleepsKatz");
+if (a==29) go_to("/annasbookcorner/historical.htm#StarSea");
+if (a==30) go_to("/annasbookcorner/contemporary.htm#Uninvited");
+if (a==31) go_to("/annasbookcorner/thriller.htm#VendettaDefence");
+if (a==32) go_to("/annasbookcorner/relationship.htm#WaterBreather");
+if (a==33) go_to("/annasbookcorner/historical.htm#LovingChe");
+if (a==34) go_to("/annasbookcorner/satire.htm#Incompetence");
+if (a==35) go_to("/annasbookcorner/thriller.htm#MoneyBurn");
+if (a==36) go_to("/annasbookcorner/contemporary.htm#OurLady");
+if (a==37) go_to("/annasbookcorner/nonfiction.htm#Quirkyalone");
+if (a==38) go_to("/annasbookcorner/satire.htm#SortingBilly");
+}
+// End -->
